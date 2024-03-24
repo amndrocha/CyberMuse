3d1f5e578f482d07e6a2d8c4b412e32b2e98eafa
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-03-24 01:10:04 -0300

    add i18n and assets

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import { BrowserRouter } from "react-router-dom";
 import store from './store';
 import { Provider } from 'react-redux';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import { supabase } from '../supabase';
 import './Home.scss'
 import { useSelector } from 'react-redux';
+import { Trans, useTranslation } from 'react-i18next';
 
 function Home() {
+  const { t, i18n } = useTranslation();
   const language = useSelector(state => state.shared.language);
   const [input, setInput] = useState('');
   const heart = '<3';
   const [content, setContent] = useState('ok');
+
+  const lngs = {
+    en: { nativeName: 'English' },
+    de: { nativeName: 'Deutsch' }
+  };
 
   async function insertEmail() {
     if (input !== '') {
@@ -31,24 +38,39 @@ function Home() {
   return (
       <div className='page'>
         <div className='column'>
-          <div className='logo-box'>
-            <div className='logo'>
-              <img className='logo-stars' src='./assets/stars.png'/>
-              <div className='cyber'>Cyber</div>
-              <div className='muse'>Muse</div>
+            <div className='logo-box'>
+                <div className='logo'>
+                <img className='logo-stars' src='./assets/stars.png'/>
+                <div className='cyber'>Cyber</div>
+                <div className='muse'>Muse</div>
+                </div>
             </div>
-          </div>
 
-          <div>{language}</div>
+            <div>{language}</div>
+            <div>
+                {Object.keys(lngs).map((lng) => (
+                <button key={lng} type="submit" 
+                style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} 
+                onClick={() => i18n.changeLanguage(lng)}>
+                    {lngs[lng].nativeName}
+                </button>
+                ))}
+            </div>
+            <p>
+                <Trans i18nKey="description.part1">
+                Edit <code>src/App.js</code> and save to reload.
+                </Trans>
+            </p>
+            {t('description.part2')}
 
-          <div className='introduction'>
-            Um novo jogo de moda vem aí. Nos informe o seu e-mail abaixo e seja o primeiro a saber!
-          </div>
-            
-          <div className='input-box'>
-            <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='johndoe@gmail.com'/>
-            <button onClick={insertEmail}>{content} {heart}</button>
-          </div>
+            <div className='introduction'>
+                Um novo jogo de moda vem aí. Nos informe o seu e-mail abaixo e seja o primeiro a saber!
+            </div>
+
+            <div className='input-box'>
+                <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='johndoe@gmail.com'/>
+                <button onClick={insertEmail}>{content} {heart}</button>
+            </div>
         </div>
 
 
