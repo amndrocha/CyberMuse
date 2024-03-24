b7b7cede3a77834f97e5f0062798f51d600c1626
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-03-23 23:39:04 -0300

    removed more mistakes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,7 +8,6 @@ export default function Navbar() {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </center>
         </div>
