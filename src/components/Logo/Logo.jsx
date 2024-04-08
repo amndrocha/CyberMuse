34f32d5a943b6283e4a8707bc311f90188393141
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-04-08 17:05:37 -0300

    temporary logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,12 +3,16 @@ import './Logo.scss';
 export function Logo() {
     return (        
       <div className='max-width fit-height'>
-        <div className='logo-text cyber'>
+        <img className='dark-hide' src='./assets/cyber.svg'/>
+        <img className='dark-hide' src='./assets/muse.svg'/>
+        <img className='light-hide' src='./assets/cyber2.svg'/>
+        <img className='light-hide' src='./assets/muse2.svg'/>
+        {/* <div className='logo-text cyber'>
           <img className='opacity-0' src='./assets/cyber.svg'/>
         </div>
         <div className='logo-text muse'>
           <img className='opacity-0' src='./assets/muse.svg'/>
-        </div>
+        </div> */}
       </div>
     )
 }
