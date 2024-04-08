0108af367e139bf05f58c0e4e6045f2f96b7a395
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-04-08 19:38:08 -0300

    test mask on mobile

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,16 +3,16 @@ import './Logo.scss';
 export function Logo() {
     return (        
       <div className='max-width fit-height'>
-        <img className='dark-hide' src='./assets/cyber.svg'/>
+        {/* <img className='dark-hide' src='./assets/cyber.svg'/>
         <img className='dark-hide' src='./assets/muse.svg'/>
         <img className='light-hide' src='./assets/cyber2.svg'/>
-        <img className='light-hide' src='./assets/muse2.svg'/>
-        {/* <div className='logo-text cyber'>
+        <img className='light-hide' src='./assets/muse2.svg'/> */}
+        <div className='logo-text cyber'>
           <img className='opacity-0' src='./assets/cyber.svg'/>
         </div>
         <div className='logo-text muse'>
           <img className='opacity-0' src='./assets/muse.svg'/>
-        </div> */}
+        </div>
       </div>
     )
 }
