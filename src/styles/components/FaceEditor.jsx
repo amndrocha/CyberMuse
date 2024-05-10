9dbb48aefccb204d8305bcb5a929f752e5c0aaaa
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-05-09 21:52:02 -0300

    add button to download face

--- src/styles/components/FaceEditor.jsx
+++ src/styles/components/FaceEditor.jsx
@@ -1,9 +1,30 @@
-import './FaceEditor.scss'
+import { toPng } from 'html-to-image';
+import './FaceEditor.scss';
+import { useCallback, useRef } from 'react';
 
 function FaceEditor() {
+  const ref = useRef()
+
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return
+    }
+
+    toPng(ref.current, { cacheBust: true, })
+      .then((dataUrl) => {
+        const link = document.createElement('a')
+        link.download = 'my-image-name.png'
+        link.href = dataUrl
+        link.click()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [ref])
+
   return (
     <div className='face-wrapper'>
-      <div className="face-frame">
+      <div className="face-frame" ref={ref}>
         <div className='face-shade'></div>
         <div className="eyes-wrapper mirror">
           <div className='eyes'>
@@ -28,9 +49,9 @@ function FaceEditor() {
           </div>
           <img className="opacity-0 eye" src='./items/eye-shape.png'/>
         </div>
-      </div>      
+      </div>
+      <button style={{fontSize: '12px', marginTop: '20px'}} onClick={onButtonClick}>Download Face</button>
     </div>
-    
   );
 }
 
