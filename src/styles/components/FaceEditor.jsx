367b9f5becc34fc7c86545b5548adbad34e830be
Amanda Rocha Leite de Araújo <amndrocha@gmail.com>
2024-05-16 19:25:22 -0300

    moving eye height

--- src/styles/components/FaceEditor.jsx
+++ src/styles/components/FaceEditor.jsx
@@ -1,34 +1,74 @@
 import { toPng } from 'html-to-image';
 import './FaceEditor.scss';
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function FaceEditor() {
-  const ref = useRef()
+  const sensitivity = 0.1;
+  const [eyeGap, setEyeGap] = useState(20);
+  const [eyeHeight, setEyeHeight] = useState(70);
+  const [initialPos, setInitialPos] = useState(0);
+  const ref = useRef();
+  const [isDragging, setIsDragging] = useState(false);
+  const [triggered, setTriggered] = useState(null);
+ 
+  const triggerEyeGap = (e) => {
+    setTriggered('eyeGap');
+    setInitialPos(e.clientX - ref.current.getBoundingClientRect().left);
+    setIsDragging(true);
+  };
 
-  const onButtonClick = useCallback(() => {
+  const triggerEyeHeight = (e) => {
+    setTriggered('eyeHeight');
+    setInitialPos(e.clientY - ref.current.getBoundingClientRect().top);
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      if (triggered === 'eyeGap') {
+        const newEyeGap = (e.clientX - ref.current.getBoundingClientRect().left - initialPos) * sensitivity;
+        if (eyeGap + newEyeGap > 10) {
+          setEyeGap(eyeGap + newEyeGap);
+        }
+      }
+      if (triggered === 'eyeHeight') {
+        const newEyeHeight = (e.clientY - ref.current.getBoundingClientRect().top - initialPos) * 0.03;
+        if (eyeHeight + newEyeHeight > 0) {
+          setEyeHeight(eyeHeight + newEyeHeight);
+        }
+      }
+    }
+  };
+
+  const downloadImage = useCallback(() => {
     if (ref.current === null) {
-      return
+      return;
     }
 
-    toPng(ref.current, { cacheBust: true, })
+    toPng(ref.current, { cacheBust: true })
       .then((dataUrl) => {
-        const link = document.createElement('a')
-        link.download = 'my-image-name.png'
-        link.href = dataUrl
-        link.click()
+        const link = document.createElement('a');
+        link.download = 'my-image-name.png';
+        link.href = dataUrl;
+        link.click();
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [ref])
+        console.log(err);
+      });
+  }, [ref]);
+
 
   return (
     <div className='face-wrapper'>
-      <div className="face-frame" ref={ref}>
+      <div className="face-frame" ref={ref} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
         <div className='face-shade'></div>
-        <div className="eyes-wrapper mirror">
+        <div id='eyes' className="eyes-wrapper mirror" style={{ top: eyeHeight + 'px' }}>
           <div className='eyes'>
-            <div className="mirrored eye">
+            <div className="mirrored eye" onMouseDown={triggerEyeHeight}>
               <div className='eyeball'>
                 <div className='iris'>
                   <img className='iris-base' src="./items/iris-base.png"/>
@@ -37,7 +77,8 @@ function FaceEditor() {
                 </div>
               </div>
             </div>
-            <div className="eye">
+            <div className='eye-gap' style={{ width: eyeGap + 'px' }} onMouseDown={triggerEyeGap}></div>
+            <div className="eye" onMouseDown={triggerEyeHeight}>
               <div className='eyeball'>
                 <div className='iris'>
                   <img className='iris-base' src="./items/iris-base.png"/>
@@ -50,7 +91,7 @@ function FaceEditor() {
           <img className="opacity-0 eye" src='./items/eye-shape.png'/>
         </div>
       </div>
-      <button style={{fontSize: '12px', marginTop: '20px'}} onClick={onButtonClick}>Download Face</button>
+      <button style={{ fontSize: '12px', marginTop: '20px' }} onClick={downloadImage}>Download Face</button>
     </div>
   );
 }
